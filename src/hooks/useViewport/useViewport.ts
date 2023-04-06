146204e08c39916458d197f83f7f4608f2df6ff6
Josh Mitchell <jlmitchwd@gmail.com>
2023-04-06 18:08:09 -0400

    More updates.

--- src/hooks/useViewport/useViewport.ts
+++ src/hooks/useViewport/useViewport.ts
@@ -1,9 +1,22 @@
 import { useEffect, useState } from 'react';
 
 export const useViewport = () => {
+  const isBrowser = typeof window !== "undefined";
+
+  if (!isBrowser) {
+    // to fix the error: "window is not defined"
+    // https://blog.greenroots.info/gatsby-the-window-is-not-defined-error-what-and-how-to-fix-it
+    return { screenWidth: 0 };
+  }
+
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
   useEffect(() => {
+    if (!isBrowser) {
+      setScreenWidth(0);
+      return;
+    }
+
     const handleWindowResize = () => setScreenWidth(window.innerWidth);
 
     window.addEventListener("resize", handleWindowResize);
